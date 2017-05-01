83d3b9fd8097076d4909e343ba624e9f30a4048b
Yangwoo Lee <lyw0149@gmail.com>
2017-05-01 15:44:50 +0900

    Delete subdomain package.

--- server/index.js
+++ server/index.js
@@ -2,15 +2,11 @@
 import path from 'path';
 import webpack from 'webpack';
 import express from 'express';
-import subdomain from 'express-subdomain';
 import WebpackDevServer from 'webpack-dev-server';
 import historyApiFallback from 'connect-history-api-fallback';
 import chalk from 'chalk';
 import webpackConfig from '../webpack.config';
 import config from '../config/environment';
-
-const router = express.Router();
-const subdomainString = 'admin';
 
 if (config.env === 'development') {
   const app = new WebpackDevServer(webpack(webpackConfig), {
@@ -23,14 +19,12 @@ if (config.env === 'development') {
   });
 
   // Serve static resources
-  router.get('/', express.static(path.join(__dirname, '../build')));
-  app.use(subdomain(subdomainString, router));
+  app.use('/', express.static(path.join(__dirname, '../build')));
   app.listen(config.port, () => console.log(chalk.green(`listening on port ${config.port}`)));
 } else if (config.env === 'production') {
   // Launch Relay by creating a normal express server
   const app = express();
   app.use(historyApiFallback());
-  router.get('/', express.static(path.join(__dirname, '../build')));
-  app.use(subdomain(subdomainString, router));
+  app.use('/', express.static(path.join(__dirname, '../build')));
   app.listen(config.port, () => console.log(chalk.green(`listening on port ${config.port}`)));
 }
